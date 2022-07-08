8f55f1ae920b514fbc16fa5d880ffc06c8527fe6
Luke <luke.goddard44@gmail.com>
2022-07-08 10:42:37 +0100

    Add and basic test of tree building algorithm

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,3 +1,18 @@
 class Tree
+  attr_accessor :root
+  def initialize(arr)
+    @root = build_tree(arr.sort.uniq)
+  end
 
+  def build_tree(arr)
+    if arr.length <= 2
+      root = arr.pop
+      left = arr.empty? ? nil : Node.new(arr.pop)
+      return Node.new(root, left)
+    end
+    mid = arr.length / 2
+    left = arr[0..mid - 1]
+    right =  arr[mid + 1..-1]
+    Node.new(arr[mid], build_tree(left), build_tree(right))
+  end
 end
--- main.rb
+++ main.rb
@@ -1,9 +1,6 @@
 require_relative 'lib/tree'
 require_relative 'lib/node'
 
-one = Node.new(1)
-two = Node.new(2)
-three = Node.new(3)
-
-puts one < three
-puts three < two
+tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345])
+p tree.root.left_node.left_node
+p tree.root.right_node
