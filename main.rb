f382664ba270bd85427a4f67a9e772a5c7a56e31
Luke <luke.goddard44@gmail.com>
2022-07-12 20:03:23 +0100

    Add test script for random data

--- main.rb
+++ main.rb
@@ -1,18 +1,21 @@
 require_relative 'lib/tree'
 require_relative 'lib/node'
 
-tree = Tree.new([1, 2, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 6, 10, 12, 78, 1231])
-
-p tree.balanced?
-
-tree.insert(100)
+tree = Tree.new(Array.new(15) { rand(1..100) })
+tree.pretty_print
+puts tree.balanced?
+p tree.level_order
+p tree.preorder
+p tree.postorder
+p tree.inorder
 tree.insert(101)
 tree.insert(102)
 tree.insert(103)
 tree.insert(104)
-
-tree.pretty_print
-p tree.balanced?
+puts tree.balanced?
 tree.rebalance
-tree.pretty_print
-p tree.balanced?
+puts tree.balanced?
+p tree.level_order
+p tree.preorder
+p tree.postorder
+p tree.inorder
